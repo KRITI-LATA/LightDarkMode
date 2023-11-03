dc2d04e1e64de99c5bc1a57665332837eb6b388d
kriti <kr2011la@gmail.com>
2023-11-03 08:58:17 +0000

    Dark Light Mode Committed

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,38 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+class LightDarkMode extends Component {
+  state = {isDarkMode: false}
+
+  onChangeMode = () => {
+    const {isDarkMode} = this.state
+    console.log({isDarkMode})
+    this.setState(() => ({isDarkMode: !isDarkMode}))
+  }
+
+  render() {
+    const {isDarkMode} = this.state
+
+    const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
+
+    return (
+      <div className="bg-container">
+        <div className="container { isDarkMode ? 'dark-mode' : 'light-mode'}">
+          <h1 className="head-text">Click To Change Mode</h1>
+          <div>
+            <button
+              type="button"
+              className="light-btn"
+              onClick={this.onChangeMode}
+            >
+              {buttonText}
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default LightDarkMode
